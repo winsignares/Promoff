92ee73e4b307914352b3ea178e13c3aa3ceb3466
= <=>
2021-12-25 08:47:03 -0500

    iniciamos commit para crear las diferentes ramas del proyecyos

---
+++ Components/Splashscreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Splashscreen() {
+    return (
+        <View>
+            <Text>
+                este es el splash
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
